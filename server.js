427ce397939b672736f128dbea69ba9c15aed8e7
francescoottaviani <f.ottaviani5@campus.uniurb.it>
2020-08-26 12:31:02 +0200

    Start_work

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const bodyparser = require('body-parser');
 const cookieparser = require("cookie-parser");
 const http = require('http');
